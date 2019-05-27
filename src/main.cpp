4fec513619884bc8f9d9e564d4f70350be33952b
Daniele Tabanella <daniele.tabanella@gmail.com>
2019-05-27 23:22:23 +0200

    feat: add neopixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,97 @@
-#include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <NeoPixelBus.h>
+
+const uint16_t PixelCount = 10; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 20;  // make sure to set this to the correct pin, ignored for Esp8266
+
+#define colorSaturation 128
+
+NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, PixelPin);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+HslColor hslRed(red);
+HslColor hslGreen(green);
+HslColor hslBlue(blue);
+HslColor hslWhite(white);
+HslColor hslBlack(black);
+
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial); // wait for serial attach
+
+    Serial.println();
+    Serial.println("Initializing...");
+    Serial.flush();
+
+    // this resets all the neopixels to an off state
+    strip.Begin();
+    strip.Show();
+
+
+    Serial.println();
+    Serial.println("Running...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+    delay(1000);
+
+    Serial.println("Colors R, G, B, W...");
+
+    // set the colors,
+    // if they don't match in order, you need to use NeoGrbFeature feature
+    strip.SetPixelColor(0, red);
+    strip.SetPixelColor(1, green);
+    strip.SetPixelColor(2, blue);
+    strip.SetPixelColor(3, white);
+    // the following line demonstrates rgbw color support
+    // if the NeoPixels are rgbw types the following line will compile
+    // if the NeoPixels are anything else, the following line will give an error
+    //strip.SetPixelColor(3, RgbwColor(colorSaturation));
+    strip.Show();
+
+
+    delay(1000);
+
+    Serial.println("Off ...");
+
+    // turn off the pixels
+    strip.SetPixelColor(0, black);
+    strip.SetPixelColor(1, black);
+    strip.SetPixelColor(2, black);
+    strip.SetPixelColor(3, black);
+    strip.Show();
+
+    delay(1000);
+
+    Serial.println("HSL Colors R, G, B, W...");
+
+    // set the colors,
+    // if they don't match in order, you may need to use NeoGrbFeature feature
+    strip.SetPixelColor(0, hslRed);
+    strip.SetPixelColor(1, hslGreen);
+    strip.SetPixelColor(2, hslBlue);
+    strip.SetPixelColor(3, hslWhite);
+    strip.Show();
+
+
+    delay(1000);
+
+    Serial.println("Off again...");
+
+    // turn off the pixels
+    strip.SetPixelColor(0, hslBlack);
+    strip.SetPixelColor(1, hslBlack);
+    strip.SetPixelColor(2, hslBlack);
+    strip.SetPixelColor(3, hslBlack);
+    strip.Show();
+
 }
